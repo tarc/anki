fe715584d12455de31d48aa2c0e16f6a286fb740
Tarcísio Genaro Rodrigues <tarcisio.genaro@gmail.com>
2023-01-04 06:30:35 -0300

    Add initial Nix Flakes setup

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+    let
+      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
+    in
+    {
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = [
+                    pkgs.git
+                    pkgs.darwin.apple_sdk.frameworks.Security
+                    pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+                    pkgs.libiconv
+                    pkgs.ninja
+                    pkgs.openssl.dev
+                  ];
+
+                  env.RUSTFLAGS = (builtins.map (l: ''-L ${l}/lib'') [
+                    pkgs.libiconv
+                    pkgs.openssl.dev
+                  ]) ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin
+                    (with pkgs.darwin.apple_sdk; builtins.map (l: ''-L framework=${l}/Library/Frameworks'') [
+                      frameworks.CoreFoundation
+                      frameworks.Security
+                  ]));
+
+                  languages.rust = {
+                    enable = true;
+                    version = "stable";
+                  };
+
+                  enterShell = ''
+                    git --version
+                  '';
+                }
+              ];
+            };
+          });
+    };
+}
